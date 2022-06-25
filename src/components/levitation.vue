a7590fa66c282c2df4c11274e5063be75f3ed031
Simon he <13917107460@163.com>
2022-06-25 15:35:58 +0800

    chore: update

--- src/components/levitation.vue
+++ src/components/levitation.vue
@@ -1,16 +1,54 @@
 <script setup lang="ts">
 import { preload } from 'simon-js-tool'
-preload('/images/portrait.png')
+preload([
+  '/images/portrait.png',
+  '/images/yellow.png',
+  '/images/red.png',
+  '/images/blue.png',
+  '/images/purple.png',
+])
 const el = ref(null)
+const wrapperEl = ref(null)
+const color = ref('white')
+const backgroundImage = ref('url(/images/R.png)')
 function animationend() {
-  el.value.style.backgroundImage = 'url(/images/portrait.png)'
+  backgroundImage.value = 'url(/images/portrait.png)'
+  setTimeout(yellow, 1000)
+}
+function yellow() {
+  color.value = '#FEF222'
+  backgroundImage.value = 'url(/images/yellow.png)'
+  setTimeout(red, 1000)
+}
+function red() {
+  color.value = '#8D022B'
+  backgroundImage.value = 'url(/images/red.png)'
+  setTimeout(blue, 1000)
+}
+function blue() {
+  color.value = '#3AEFFB'
+  backgroundImage.value = 'url(/images/blue.png)'
+  setTimeout(purple, 1000)
+}
+function purple() {
+  color.value = '#CF78EB'
+  backgroundImage.value = 'url(/images/purple.png)'
+  setTimeout(yellow, 1000)
 }
 </script>
 
 <template>
   <section class="lg:w-1/3 flex flex-wrap items-center" fixed left-0 bottom-10>
-    <div class="astronaut-wrapper" @animationend="animationend">
-      <div ref="el" class="astronaut" />
+    <div
+      ref="wrapperEl"
+      class="astronaut-wrapper"
+      :style="{
+        color: 'red',
+        filter: 'drop-shadow(2px 4px 6px ' + color + ')',
+      }"
+      @animationend="animationend"
+    >
+      <div ref="el" class="astronaut" :style="{ backgroundImage: backgroundImage }" />
     </div>
   </section>
 </template>
@@ -27,18 +65,15 @@ function animationend() {
     40.001% {
       transform: translate(100vw, 40vh) rotate(1turn) scale(0.2);
       transform-origin: center -100%;
-      filter: drop-shadow(2px 4px 6px red);
     }
     80% {
       transform: translate(50vw, -100vh) rotate(-20turn) scale(0.2);
     }
     95% {
       transform: translate(25vw, -50vh) rotate(-20turn) scale(0.2);
-      filter: drop-shadow(2px 4px 6px gold);
     }
     100% {
       transform: translateX(0) rotate(0) scale(1);
-      filter: drop-shadow(2px 4px 6px gold);
     }
   }
   @keyframes levitation {
@@ -59,7 +94,6 @@ function animationend() {
     animation: flyaway 60s 0s ease-in 1 forwards;
   }
   .astronaut {
-    background-image: url("/images/pic.svg");
     width: 15vmax;
     height: 10vmax;
     position: relative;
