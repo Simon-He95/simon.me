6de593726b2856265c790f2b3fefafeb37a40046
Simon he <13917107460@163.com>
2022-06-29 15:24:17 +0800

    chore: add dotImage

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
-import { DotTextCanvas } from 'simon-js-tool'
+import { DotImageCanvas, DotTextCanvas } from 'simon-js-tool'
 import { onBeforeRouteUpdate, useRouter } from 'vue-router'
 import { isDark } from '~/logics'
+import lufei from '/images/lufei.png'
 useHead({
   meta: [
     { property: 'og:title', content: 'Simon He' },
@@ -14,9 +15,13 @@ useHead({
 })
 const text = ref('')
 const dotText = new DotTextCanvas(text.value, 20, isDark.value ? 'white' : 'black', 10)
-const el = ref(null)
+const dotImage = new DotImageCanvas(lufei, 2)
+const el = ref<HTMLElement>(null)
+const imageEl = ref(null)
 onMounted(() => {
   el.value?.appendChild(dotText.canvas)
+  dotImage.canvas.style.width = '10rem'
+  imageEl.value?.appendChild(dotImage.canvas)
 })
 watch(isDark, update)
 const router = useRouter()
@@ -51,6 +56,7 @@ function update() {
 </script>
 
 <template>
+  <span ref="imageEl" fixed top-20 w-100 h-100 lt-md:hidden />
   <span ref="el" fixed bottom-5 right-0 />
   <NavBar />
   <main class="px-7 py-10" overflow-x-hidden>
