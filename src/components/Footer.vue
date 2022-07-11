6a8bff4568f4ec0b2877b8435e12aa1f1e08ad02
Simon he <13917107460@163.com>
2022-07-11 20:42:01 +0800

    chore: update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="mt-10 mb-6 prose m-auto opacity-50 flex" items-center>
-    <a text-sm class="banquan" href="https://beian.miit.gov.cn/" target="_blank"
-      >沪ICP备20020389号-1 © Simon He</a
-    >
+    <a text-sm class="banquan" href="https://beian.miit.gov.cn/" target="_blank">沪ICP备20020389号-1 © Simon He</a>
     <div class="flex-auto" />
   </div>
 </template>
