744d286682d0d3d2948f94228c7c5d9b0bbe552d
Simon He <674949287@qq.com>
2025-12-23 16:24:07 +0800

    feat: add new friend entry for Chris Kang with avatar and blog link

--- src/components/FriendsList.vue
+++ src/components/FriendsList.vue
@@ -229,6 +229,13 @@ const friends = [
     description: '前端开发，并且会一些图像处理和音视频算法；全栈也能做，总要实现自己的天马行空',
     tag: 'Web Developer | Oler',
   },
+  {
+    name: 'Chris Kang',
+    avatar: 'https://kangchainx.github.io/assets/avatar/x.jpeg',
+    blog: 'https://kangchainx.com/zh',
+    description: 'Code anywhere, seek everywhere.',
+    tag: 'Full-stack developer. Coding with AI',
+  },
 ]
 
 const friends1 = ref<any>([])
